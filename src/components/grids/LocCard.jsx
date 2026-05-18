38c0dae954fa7b37a70e6fd6c4de6ec9a3887f17
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2026-05-18 14:58:50 -0300

    commit LocCards

--- src/components/grids/LocCard.jsx
+++ src/components/grids/LocCard.jsx
@@ -1,16 +1,16 @@
 export default function LocCard({img, imgAlt, eyebrow, title, pricing, url}){
     return(
-        <div>
-      <img className="rounded-lg" src={img} alt={imgAlt} />
-      <div className="mt-4">
-        <div className="text-xs font-bold text-sky-500">{eyebrow}</div>
-        <div className="mt-1 font-bold text-gray-700">
-          <a href={url} className="hover:underline">
-            {title}
-          </a>
+      <div>
+        <img className="rounded-t-xl" width={400} height={200} src={img} alt={imgAlt}/>
+        <div className=" rounded-b-xl py-4 px-4 bg-white shadow-xl">
+          <div className="text-xs font-bold text-sky-500">{eyebrow}</div>
+          <div className="mt-1 font-bold text-gray-700">
+            <a href={url} className="hover:underline">
+              {title}
+            </a>
+          </div>
+          <div className="mt-2 text-sm text-gray-600">{pricing}</div>
         </div>
-        <div className="mt-2 text-sm text-gray-600">{pricing}</div>
-      </div>
     </div>
     )
 }
