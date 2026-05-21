25f8e2dc100d1fac23ca6b71497d84a7d0e12006
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2026-05-21 17:49:44 -0300

    tela de análise e tela de locação

--- src/components/grids/LocCard.jsx
+++ src/components/grids/LocCard.jsx
@@ -1,51 +1,53 @@
 import { FaSearch } from "react-icons/fa";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faBed, faDog } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faBed, faDog, faBath, faBorderAll, faWifi, faVenus, faBoltLightning, faDroplet, faElevator, faStairs, faSquareParking, faPuzzlePiece} from '@fortawesome/free-solid-svg-icons';
 import ButtonForms from "../buttons/ButtonForms";
 import Link from "next/link";
 
 
 export default function LocCard({imovel}){
     return(
-      <div className="rounded-3xl overflow-hidden shadow-xl bg-white hover:scale-101 transition">
-        <div className="relative">
-        <img className="rounded-t-3xl w-[400px] h-[200px] object-cover" src={imovel.img} alt={imovel.imgAlt} />
 
-        {imovel.approvement === "Undefined" && (
+      <Link href={imovel.approvement === "Undefined" ? `/locacoes/analise/${imovel.id}` : `/locacoes/${imovel.id}`} className="hover:underline">
+        <div className="rounded-3xl overflow-hidden shadow-xl bg-white hover:scale-101 transition">
+          <div className="relative">
+          <img className="rounded-t-3xl w-[400px] h-[200px] object-cover" src={imovel.img} alt={imovel.imgAlt} />
 
-          <div className="absolute top-3 right-3 flex flex-row gap-2">
-            <button className="bg-[#E1DFFF] text-[#004AE5] text-xs hover:scale-105 font-bold px-5 py-2 rounded-2xl shadow-md transition">
-              Recusar
-            </button>
-            <button className="bg-linear-to-r from-[#1B3B99] to-[#819BFF] text-white text-xs hover:scale-105 font-semibold px-6 py-2 rounded-2xl shadow-md transition">
-              Aceitar
-            </button>
+            {imovel.approvement === "Undefined" && (
+
+              <div className="absolute top-3 right-3 flex flex-row gap-2">
+                <button className="bg-[#E1DFFF] text-[#004AE5] text-xs hover:scale-105 font-bold px-5 py-2 rounded-2xl shadow-md transition">
+                  Recusar
+                </button>
+                <button className="bg-linear-to-r from-[#1B3B99] to-[#819BFF] text-white text-xs hover:scale-105 font-semibold px-6 py-2 rounded-2xl shadow-md transition">
+                  Aceitar
+                </button>
+              </div>
+            )}
           </div>
-        )}
-      </div>
 
-      <div className="rounded-b-3xl py-4 px-4 bg-white shadow-xl">
-        <div className="flex flex-row">
-          <div className="mt-1 font-bold text-gray-700">
-            <Link href={imovel.approvement === "Undefined" ? `/locacoes/analise/${imovel.id}` : `/home` } className="hover:underline">
-              {imovel.title}
-            </Link>
-          </div>
-          <div className="text-md font-extrabold text-blue-500 py-1 ml-auto">{imovel.pricing}</div>
-        </div>
-
-        <div className="mt-2 text-md text-gray-600">{imovel.address}</div>
-        <div className="flex flex-row mt-4 mb-2">
-          {imovel.icons?.map((icon, index) => (
-            <div key={index} className="flex items-center mr-2 border border-black py-1 px-2 rounded-xl">
-              <FontAwesomeIcon icon={icon} className="text-black mr-1 w-[16px] h-[16px]"/>
-              <span className="text-xs text-black">
-                {icon === faHouse ? "Casa" : icon === faBed ? "2" : icon === faDog ? "Pets" : ""}
-              </span>
+          <div className="rounded-b-3xl py-4 px-4 bg-white shadow-xl">
+            <div className="flex flex-row">
+              <div className="mt-1 font-bold text-gray-700">
+                {imovel.title}
+              </div>
+              <div className="text-md font-extrabold text-blue-500 py-1 ml-auto">{imovel.pricing}</div>
             </div>
-          ))}
+
+            <div className="mt-2 text-md text-gray-600">{imovel.address}</div>
+            <div className="flex flex-row mt-4 mb-2">
+              {imovel.homeIcons?.map((icon, index) => (
+                <div key={index} className="flex items-center mr-2 border border-black py-1 px-2 rounded-xl">
+                  <FontAwesomeIcon icon={icon} className="text-black mr-1 w-[16px] h-[16px]"/>
+                  <span className="text-xs text-black">
+                    {icon === faHouse ? "Casa" : icon === faBed ? "2" : icon === faDog ? "Pets" : icon === faBath ? "2" : icon === faBorderAll ? "40m²" : ""}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </Link>
+      
     )
 }
--- src/components/home/SearchBar.jsx
+++ src/components/home/SearchBar.jsx
@@ -11,7 +11,7 @@ export default function SearchBar(){
                 <span className="text-[#A29F9F] text-md">FILTRAGEM</span>
                 <span className="text-[#545F71]">Quais as características do imóvel ou do contrato?</span>
             </div>
-            <div className="py-3 px-5 flex flex-row items-center justify-center bg-linear-to-r from-[#1B3B99] to-[#819BFF] rounded-3xl gap-4"><Image src="/images/search.png" alt="Lupa" width={18} height={12}/><span className="font-bold">Pesquisar</span></div>
+            <button className="py-3 px-5 flex flex-row items-center justify-center bg-linear-to-r from-[#1B3B99] to-[#819BFF] rounded-3xl gap-4 hover:scale-102 transition"><Image src="/images/search.png" alt="Lupa" width={16} height={12}/><span className="font-bold text-white">Pesquisar</span></button>
         </div>
     )
 }
