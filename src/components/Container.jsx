6039c010a1c8f67d2e9df24ad6d4b8ea270ce7cb
claraacv <145117941+claraacv@users.noreply.github.com>
2026-05-22 19:54:43 -0300

    Revert "Commit da apresentação"
    This reverts commit 90ce5a6e866b87b9109f55e2ad64bb6d6d04daa2.

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 function Container({children}){
     return(
-        <div className="flex flex-col w-full max-w-6xl mx-auto px-4 py-12 gap-10 justify-center bg-[#F8F5FF]">
+        <div className="flex flex-col w-full max-w-6xl mx-auto px-4 py-12 gap-10 justify-center">
             {children}
         </div>
     )
