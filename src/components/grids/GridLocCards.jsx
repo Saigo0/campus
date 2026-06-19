f26c9fd057b0c1bb4767a8983c99e5f37f040636
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2026-06-19 17:42:26 -0300

    fix gridloccard responsiveness

--- src/components/grids/GridLocCards.jsx
+++ src/components/grids/GridLocCards.jsx
@@ -2,7 +2,7 @@ import LocCard from "./LocCard";
 
 export default function GridLocCards({ imoveis }){
     return(
-        <div className= "grid grid-cols-2 sm:grid-cols-2 py-12 gap-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 py-12 gap-10">
             {imoveis.map((imovel) => (
                 <LocCard
                     key={imovel.id}
