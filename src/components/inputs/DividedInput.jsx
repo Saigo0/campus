39072da279d1a7b17ca729c46cd8f18ad9a12443
claraacv <145117941+claraacv@users.noreply.github.com>
2026-07-03 17:51:42 -0300

    Responsivity bug

--- src/components/inputs/DividedInput.jsx
+++ src/components/inputs/DividedInput.jsx
@@ -1,6 +1,6 @@
 function DividedInput({children}){
     return(
-        <div className="flex flex-col min-[900px]:flex-row sm:gap-5 w-full">
+        <div className="flex flex-col md:flex-row md:gap-5 w-full">
             {children}
         </div>
     )
